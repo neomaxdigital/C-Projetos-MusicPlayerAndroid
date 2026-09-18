34d922bfd2580af64483f0fffce9efe6c51c1028
neomaxdigital <neomaxdigital@gmail.com>
2026-09-17 23:59:32 -0300

    Restore Now Playing caller to pre-inset state

--- app/src/main/java/com/musicplayer/offline/MainActivity.kt
+++ app/src/main/java/com/musicplayer/offline/MainActivity.kt
@@ -338,7 +338,6 @@ private fun MusicPlayerApp(viewModel: MusicPlayerViewModel) {
                     openEqualizer = { feature = FeatureRoute.EQUALIZER },
                     openLyrics = { feature = FeatureRoute.LYRICS },
                     requestConversion = { song, shareAfter -> conversionRequest = ConversionRequest(song, shareAfter) },
-                    artworkSize = state.settings.artworkSize,
                     back = { nowPlaying = false }
                 )
                 else -> HomeShell(
